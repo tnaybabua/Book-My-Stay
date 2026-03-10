f7b05afd507b44ff3eea0d77dba9e0cf77bfccfc
tnaybabua <rahultanaybabu@gmail.com>
2026-03-10 09:18:54 +0530

    UC1 Welcome Message

---
+++ App/src/mystay.java
@@ -0,0 +1,16 @@
+//version 1.0
+//author Rahul
+//Usecase 1 : Welcome Pagegit
+
+public class mystay {
+
+    public static void WelcomeMessage(){
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+
+
+    public static void main(String args[]){
+    WelcomeMessage();
+    }
+}
