fb7bc71a7c199ca917c0121c0e79a59b887ce5c4
tnaybabua <rahultanaybabu@gmail.com>
2026-04-02 09:28:50 +0530

    rahul pushed uc2

--- App/src/bookmystay.java
+++ App/src/bookmystay.java
@@ -1,10 +1,100 @@
+/**
+ * ============================================================
+ * ABSTRACT CLASS - Room
+ * ============================================================
+ *
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Description:
+ * This abstract class represents a generic hotel room.
+ */
 
-// @author Rahul Tanay
-//@version 1.0
+abstract class Room {
 
-public class bookmystay{
-    public static void main(String args[]){
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized succesfully.");
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+/**
+ * ============================================================
+ * CLASS - SingleRoom
+ * ============================================================
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+/**
+ * ============================================================
+ * CLASS - DoubleRoom
+ * ============================================================
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+/**
+ * ============================================================
+ * CLASS - SuiteRoom
+ * ============================================================
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase2RoomInitialization
+ * ============================================================
+ */
+public class bookmystay {
+
+    public static void main(String[] args) {
+
+        // Create room objects
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static availability (simple variables)
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
